994eaa3ba3636929a26fab4821b150a353eb1107
MutawaPro <mohamed2015pre@gmail.com>
2021-03-30 23:42:59 +0300

    third commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import random
 
 app = Flask(__name__)
@@ -11,14 +11,22 @@ potato_family = {
     "Sweet Potato": "Sweet-Potato.png"
 }
 
-potato_familyName = list(potato_family.keys())
-random_potato = random.choice(potato_familyName)
-potato_familyFile = potato_family[random_potato]
 
-@app.route("/")
-def home():
-    
-    return render_template('index.html', potatoName = potato_familyName, potatoFile = potato_familyFile)
+
+def generateRandomPotato():
+    potato_familyName = list(potato_family.keys())
+    random_potato = random.choice(potato_familyName)
+    potato_familyFile = potato_family[random_potato]
+    return potato_familyName, random_potato, potato_familyFile
+
+@app.route("/", methods=["POST", "GET"])
+def home(): 
+    potato_familyName, random_potato, potato_familyFile = generateRandomPotato()
+
+    if request.method == "POST":
+        if request.form.get("button_a"): 
+            potato_familyName, random_potato, potato_familyFile = generateRandomPotato()
+    return render_template('index.html', potatoName = random_potato, potatoFile = potato_familyFile)
 
 if __name__ == "__main__":
     app.run(debug=True)
