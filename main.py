d1049097ae7ca5982e563f7a84c9c4c947e6b0d1
MutawaPro <mohamed2015pre@gmail.com>
2021-03-30 23:11:11 +0300

    seconed Commit

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import random
 
 app = Flask(__name__)
 
+potato_family = {
+    "potato": "potato.png",
+    "French Fries": "French-Fries.png",
+    "Blue Swede": "blueswede.jpg",
+    "Chips": "chips.png",
+    "Sweet Potato": "Sweet-Potato.png"
+}
+
+potato_familyName = list(potato_family.keys())
+random_potato = random.choice(potato_familyName)
+potato_familyFile = potato_family[potato_familyName]
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    
+    return render_template('index.html', potatoName = potato_familyName, potatoFile = potato_familyFile)
 
 if __name__ == "__main__":
     app.run(debug=True)
